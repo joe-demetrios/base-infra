1c5ecd64bb7c6683c443ad231070254caa0495e9
Joseph Demetrios <jjdemetrios@gmail.com>
2025-09-25 22:10:00 +0100

    fix: index fix for output

--- modules/iam_role/outputs.tf
+++ modules/iam_role/outputs.tf
@@ -1,7 +1,7 @@
 output "name" {
-    value = aws_iam_role.this.name
+    value = aws_iam_role.this[0].name
 }
 
 output "arn" {
-    value = aws_iam_role.this.arn
+    value = aws_iam_role.this[0].arn
 }
